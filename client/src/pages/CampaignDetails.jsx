4e9adf6eb4059799dd639f07a1386768a832be15
João <jvfernandes.contato@gmail.com>
2024-08-12 12:14:02 -0300

    fix: fix the error of number of donators

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -126,7 +126,7 @@ const CampaignDetails = () => {
                         <div className="mt-[20px] flex flex-col gap-4">
                             {donators.length > 0 ? (
                                 donators.map((item, index) => (
-                                    <div>DONATOR</div>
+                                    <div key={index}>DONATOR</div>
                                 ))
                             ) : (
                                 <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
